8577d19d626f8432e06c9932c4aee7c9f6609972
FabioVioli <fabiovioli84@gmail.com>
2022-12-16 12:28:49 +0100

    search display

--- b/prova/page-artista/script/search.js
+++ b/prova/page-artista/script/search.js
@@ -11,13 +11,13 @@ cerca.addEventListener('click', function () {
 
   main.classList.add('container-fluid');
 
-  main.innerHTML = `<div class="row justify-content-between pt-2 pb-3">
+  main.innerHTML = `<div class="row justify-content-between pt-2 pb-3 align-items-center">
 
     <!--Frecce per moversi nella paggina-->
     <div id="frecce" class="col-7 d-flex align-items-center gap-3">
       <i class="bi bi-chevron-left ms-2 desk-arrow"></i>
       <i class="bi bi-chevron-right ms-2 desk-arrow d-none d-xl-inline"></i>
-      <div class="px-3 py-2 bg-white rounded-pill text-dark">
+      <div class="px-3 py-2 bg-white rounded-pill text-dark d-flex">
       <i class="bi bi-search"></i>
       <input type="text" class="querry border-0" placeholder='Cosa vuoi ascoltare?'/>
       <i class="bi bi-x-lg del"></i>
@@ -26,8 +26,8 @@ cerca.addEventListener('click', function () {
 
     <!--Elemento utente-->
     <div id="user-cont" class="col-5 d-flex align-items-center text-end d-none ps-4 d-xl-block">
-      <button id="user" class=" m-0 p-0">
-        <img src="../assets/img/SVG_Circle.svg.png" class="pe-1 img-fluid" alt=""> Lidia Nautilus ... <i
+      <button id="user" class=" m-0 p-0 ">
+        <img src="../assets/img/nautilus.webp" class="pe-1 img-fluid rounded-circle" alt=""> Lidia Nautilus ... <i
           class="bi bi-caret-down-fill px-2"></i></button>
     </div>
 
@@ -115,19 +115,36 @@ cerca.addEventListener('click', function () {
 function l(){
   fetch('../assets/json/categorie.json').then(response => response.json()).then(data => {
 
-    for(let i = 0; i < 4; i++){
+    for(let i = 0; i < 30; i++){
       let v = document.querySelector('#categorie .row');
-      
       let div = document.createElement("div");
       
-      div.classList.add('col-6', 'col-xl-3', 'p-3');
-      div.style.backgroundColor = data[i].color
+      div.classList.add('col-6', 'col-md-4', 'col-xl-3', 'p-3');
+      //NICO
+      //div.style.backgroundColor = data[i].color        
+    
+      //PROVA 1 
+      //   div.innerHTML = `<div class="card" style="background-color:${data[i].color};">
+      //   <img src="${data[i].img}"class="card-img-top img-fluid d-none d-lg-block">
+      //   <div class="card-body">
+      //   <p class="m-0 text-center">${data[i].name}</p>
+      //   </div>
+      // </div>`
       
-      div.innerHTML = 
-      `<div row>
-      <img class="img-fluid" src="${data[i].img}">
-      <p>${data[i].name}</p>
-      </div>`;
+      div.innerHTML = `<div class="card" style="background-color:${data[i].color}; height:140px; overflow:hidden;">
+        
+        <p class="m-0 p-2" style="font-size:1.2em; font-weight: 700;">${data[i].name}</p>
+        <img src="${data[i].img}" class="card-img-top img-fluid w-50" style="transform: rotate(25deg); position: relative; top: 35px; left: 83px;">
+        
+        
+      </div>`
+      //NICO
+      // `<div style="padding: -5px"; background-color: black;">
+      // <div row class="row">
+      // <img class="img-fluid" src="${data[i].img}">
+      // <p>${data[i].name}</p>
+      // </div>
+      // </div>`;
       
         v.append(div);
       }
